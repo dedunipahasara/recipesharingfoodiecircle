efb89d86f8f2f68618cf940f64868eb04f793f3a
dedunipahasara <thamashadeduni1030@gmail.com>
2026-04-06 03:07:07 +0530

    fix: mime type error

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+// ADD THIS: Essential for React Toastify to look correct
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
